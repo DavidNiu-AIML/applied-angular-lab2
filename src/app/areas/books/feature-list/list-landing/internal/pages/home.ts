6ed18180d4973a135a177aae82f992245425ca04
David Niu <david_niu@progressive.com>
2026-01-29 21:54:35 +0000

    got list and stats into their own file

--- src/app/areas/books/feature-list/list-landing/internal/pages/home.ts
+++ src/app/areas/books/feature-list/list-landing/internal/pages/home.ts
@@ -1,5 +1,8 @@
-import { httpResource } from '@angular/common/http';
-import { Component, ChangeDetectionStrategy, computed } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { List } from '@ht/books/feature-list/list';
+import { Stats } from '@ht/books/feature-list/stats';
+import { PageLayout } from '@ht/shared/ui-common/layouts/page';
+
 export interface book {
   title: string;
   id: string;
@@ -10,69 +13,13 @@ export interface book {
 @Component({
   selector: 'ht-home-home',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [PageLayout, List, Stats],
   template: `
-    <div class="overflow-x-auto">
-      <table class="table table-zebra">
-        <!-- head -->
-        <thead>
-          <tr>
-            <th>total books</th>
-            <th>earliest year</th>
-            <th>latest year</th>
-            <th>total pages</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{{ totalBooks() }}</td>
-            <td>{{ earliestYear() }}</td>
-            <td>{{ latestYear() }}</td>
-            <td>{{ totalPages() }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-
-    <div class="overflow-x-auto">
-      <table class="table table-zebra">
-        <!-- head -->
-        <thead>
-          <tr>
-            <th>title</th>
-            <th>id</th>
-            <th>author</th>
-            <th>year</th>
-          </tr>
-        </thead>
-        <tbody>
-          @for (book of booksResource.value(); track book.title) {
-            <tr>
-              <td>{{ book.title }}</td>
-              <td>{{ book.id }}</td>
-              <td>{{ book.author }}</td>
-              <td>{{ book.year }}</td>
-            </tr>
-          }
-        </tbody>
-      </table>
-    </div>
-    <!-- <app-ui-page title="list">
-      <pre>{{ booksResource.value() | json }}</pre>
-    </app-ui-page> -->
+    <app-ui-page title="list">
+      <app-stats />
+      <app-list />
+    </app-ui-page>
   `,
   styles: ``,
 })
-export class HomePage {
-  booksResource = httpResource<book[]>(() => '/api/books');
-  totalBooks = computed(() => this.booksResource.value()?.length ?? 0);
-  earliestYear = computed(
-    () => this.booksResource.value()?.reduce((b1, b2) => (b1.year < b2.year ? b1 : b2)).year,
-  );
-  latestYear = computed(
-    () => this.booksResource.value()?.reduce((b1, b2) => (b1.year > b2.year ? b1 : b2)).year,
-  );
-  totalPages = computed(() =>
-    (this.booksResource.value() ?? []).reduce((acc, book) => acc + book.pages, 0),
-  );
-}
+export class HomePage {}
